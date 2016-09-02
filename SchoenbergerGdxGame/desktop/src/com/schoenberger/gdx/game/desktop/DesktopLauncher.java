9055d2b34db76f423e0740432edae5764dd7c5e1
Evan Schoenberger <evanjschoenberger@gmail.com>
2016-09-02 16:00:59 -0400

    First commit

---
+++ SchoenbergerGdxGame/desktop/src/com/schoenberger/gdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.schoenberger.gdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.schoenberger.gdx.game.SchoenbergerGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SchoenbergerGdxGame(), config);
+	}
+}
